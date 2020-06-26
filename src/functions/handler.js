e544e1faa3159071e09f3ebf9b1e79983263448f
Shashank Pachava <spachava753@gmail.com>
2020-06-26 13:19:43 -0400

    Add code for versions support

--- src/functions/handler.js
+++ src/functions/handler.js
@@ -50,16 +50,18 @@ module.exports.changeState = (event, context, callback) => {
 module.exports.getState = (event, context, callback) => {
   const entryId = event.pathParameters.entryId;
   console.log(`entryId param: ${entryId}`);
+  const details = event.pathParameters.details;
+  console.log(`details param: ${details}`);
 
   console.log(`validating body`);
-  if (typeof entryId !== 'string') {
-    console.error(`Validation Failed. Type is ${typeof entryId}`);
+  if (typeof entryId !== 'string' && typeof details !== 'string' ) {
+    console.error(`Validation Failed. Type is entryId: ${typeof entryId} and details: ${typeof details}`);
     callback(new Error(`Couldn't submit entry because of validation errors.`));
     return;
   }
   console.log(`validated body`);
 
-  getEntryState(entryId)
+  getEntryState(entryId, details)
     .then(result => {
       console.log(`result returned was ${result}`);
       const response = {
@@ -78,16 +80,64 @@ module.exports.getState = (event, context, callback) => {
     });
 };
 
-const getEntryState = (entryId) => {
+module.exports.getAllInfo = (event, context, callback) => {
+  const entryId = event.pathParameters.entryId;
+  console.log(`entryId param: ${entryId}`);
+
+  console.log(`validating body`);
+  if (typeof entryId !== 'string') {
+    console.error(
+      `Validation Failed. Type is entryId: ${typeof entryId}`
+    );
+    callback(new Error(`Couldn't submit entry because of validation errors.`));
+    return;
+  }
+  console.log(`validated body`);
+
+  getAllEntryInfo(entryId)
+    .then(result => {
+      console.log(`result returned was ${result}`);
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*', // <-- Add your specific origin here
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify(result.Item)
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      console.error(error);
+      callback(new Error("Couldn't fetch candidate."));
+    });
+};
+
+const getEntryState = (entryId, details) => {
   console.log('Getting entry state');
   const itemInfo = {
     TableName: process.env.TABLE,
     Key: {
-      entryId: entryId
+      entryId: entryId,
+      details: details
     }
   };
   return dynamoDb.get(itemInfo).promise();
 };
+
+
+const getAllEntryInfo = (entryId) => {
+  console.log('Getting entry state');
+  const itemInfo = {
+    TableName: process.env.TABLE,
+    KeyConditionExpression: 'entryId = :hkey',
+    ExpressionAttributeValues: {
+      ':hkey': entryId
+    }
+  };
+  return dynamoDb.get(itemInfo).promise();
+};
+
 
 const submitEntryState = (entry) => {
   console.log('Submitting entry state');
